483e4d6f90059033a224f42bf2e9ff75fa62f47a
João Victor Ramalho Pereira <victorjudoca2009@hotmail.com>
2022-05-13 09:29:39 -0300

    first commit

---
+++ app/src/main/java/com/cursoandroidkotlin/jamiltondamasceno/frasesdodia/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.cursoandroidkotlin.jamiltondamasceno.frasesdodia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var texto: TextView
+    val frases = arrayOf(
+            "O importante não é vencer todos os dias, mas lutar sempre.",
+            "Maior que a tristeza de não haver vencido é a vergonha de não ter lutado!",
+            "É melhor conquistar a si mesmo do que vencer mil batalhas.",
+            "Enquanto houver vontade de lutar haverá esperança de vencer."
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.textoFrase) as TextView
+
+    }
+
+    fun gerarFrase(view: View){
+
+        val totalIntensArray = frases.size
+        val numeroAleatorio = Random().nextInt( totalIntensArray )
+
+        texto.setText( frases[ numeroAleatorio ] )
+
+    }
+
+}
